e351c0ba230ea199e0c845cb632b3653afa5a0eb
Nguyen Anh Quan <crossfireaz282@gmail.com>
2026-05-25 15:24:58 +0700

    Seller authen

--- Safexchange/Pages/Seller/Index.cshtml.cs
+++ Safexchange/Pages/Seller/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Safexchange.Services;
 
 namespace Safexchange.Pages.Seller;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly SafexchangeDbContext _db;
